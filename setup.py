1ea5945f52ad84cb8fd6e98987d3cdc7d756c41c
Godefroid Chapelle <gotcha@bubblenet.be>
2008-01-17 11:22:12 +0000

    missing dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='collective.rope',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'collective.lead',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
