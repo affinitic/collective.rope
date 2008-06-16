fe3c4ebb07cebad59fbf7d309a9bbd45779a9634
Godefroid Chapelle <gotcha@bubblenet.be>
2008-06-16 19:21:30 +0000

    add dependencies

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ version = '0.1'
 
 setup(name='collective.rope',
       version=version,
-      description="Zope2 support for objects built with SQLAlchemy of data stored in relational databases",
+      description="Zope2 support for objects built with SQLAlchemy of data "
+                  "stored in relational databases",
       long_description="""\
 """,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Zope2",
         "Programming Language :: Python",
@@ -25,7 +25,6 @@ setup(name='collective.rope',
       install_requires=[
           'setuptools',
           'collective.lead',
-          'collective.rope',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
