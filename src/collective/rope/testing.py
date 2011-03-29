d08ed5d9f1eaea132885c602f8167a46af6fa9f4
Godefroid Chapelle <gotcha@bubblenet.be>
2011-03-29 12:23:07 +0000

    use latest plone.testing and plone.app.testing helper layers

--- src/collective/rope/testing.py
+++ src/collective/rope/testing.py
@@ -1,5 +1,4 @@
 from plone.testing import z2
-from plone.testing import zca
 import plone.app.testing.layers
 
 import collective.rope
@@ -10,54 +9,43 @@ from collective.rope.zopetestcasecompat import ZTCCompatIntegration
 from collective.rope.zopetestcasecompat import ZTCCompatFunctional
 
 
-class RopeZcml(plone.testing.Layer):
+ROPE_FIXTURE = plone.testing.zca.ZCMLSandbox(bases=(z2.STARTUP,),
+    name="ROPE_FIXTURE",
+    filename="testingzope.zcml", package=collective.rope)
 
-    defaultBases = (z2.STARTUP, )
+
+class RopeInstall(plone.testing.Layer):
+
+    defaultBases = (ROPE_FIXTURE,)
 
     def setUp(self):
-        zca.setUpZcmlFiles((
-            ('testingzope.zcml', collective.rope),
-            ))
         with z2.zopeApp() as app:
             z2.installProduct(app, 'collective.rope')
 
     def tearDown(self):
         with z2.zopeApp() as app:
             z2.uninstallProduct(app, 'collective.rope')
-        zca.tearDownZcmlFiles()
 
-
-ROPE_FIXTURE = RopeZcml(name="RopeZcml")
+ROPE_INSTALL = RopeInstall(name="ROPE_INSTALL")
 
 ROPE_INTEGRATION = z2.IntegrationTesting(bases=(ROPE_FIXTURE,),
-    name="Rope:Integration")
+    name="ROPE_INTEGRATION")
 
 ROPE_FUNCTIONAL = z2.FunctionalTesting(bases=(ROPE_FIXTURE,),
-name="Rope:Functional")
+    name="ROPE_FUNCTIONAL")
 
 ZTC_ROPE_INTEGRATION = ZTCCompatIntegration(
     bases=(ROPE_FIXTURE, ),
-    name='ZTC:Rope:Integration')
+    name='ZTC_ROPE_INTEGRATION')
 
 ZTC_ROPE_FUNCTIONAL = ZTCCompatFunctional(
+    bases=(ROPE_INSTALL, ),
+    name='ZTC_ROPE_FUNCTIONAL')
+
+CMF_ROPE_ZCML_FIXTURE = plone.testing.zca.ZCMLSandbox(
     bases=(ROPE_FIXTURE, ),
-    name='ZTC:Rope:Functional')
-
-
-class CMFRopeZcml(plone.testing.Layer):
-
-    defaultBases = (ROPE_FIXTURE, )
-
-    def setUp(self):
-        zca.setUpZcmlFiles((
-            ('testingcmf.zcml', collective.rope),
-            ))
-
-    def tearDown(self):
-        zca.tearDownZcmlFiles()
-
-
-CMF_ROPE_ZCML_FIXTURE = CMFRopeZcml(name="CMFRopeZcml")
+    name="CMF_ROPE_ZCML_FIXTURE",
+    filename="testingcmf.zcml", package=collective.rope)
 
 
 class CMFRope(plone.testing.Layer):
@@ -70,51 +58,27 @@ class CMFRope(plone.testing.Layer):
             cmf_layers.applyProfile(portal,
                 'collective.rope.tests:ropeoncmf')
 
-CMF_ROPE_FIXTURE = CMFRope(name="CMFRope")
+CMF_ROPE_FIXTURE = CMFRope(name="CMF_ROPE_FIXTURE")
 
 CMF_ROPE_INTEGRATION = cmf_layers.IntegrationTesting(
     bases=(CMF_ROPE_FIXTURE, ),
-    name='CMFRope:Integration')
+    name='CMF_ROPE_INTEGRATION')
 
 CMF_ROPE_FUNCTIONAL = cmf_layers.FunctionalTesting(
     bases=(CMF_ROPE_FIXTURE, ),
-    name='CMFRope:Functional')
+    name='CMF_ROPE_FUNCTIONAL')
 
-
-class PloneRopeZcml(plone.testing.Layer):
-
-    defaultBases = (ROPE_FIXTURE, )
-
-    def setUp(self):
-        zca.setUpZcmlFiles((
-            ('testingplone.zcml', collective.rope),
-            ))
-
-    def tearDown(self):
-        zca.tearDownZcmlFiles()
-
-
-PLONE_ROPE_ZCML_FIXTURE = PloneRopeZcml(name="PloneRopeZcml")
-
-
-class PloneRope(plone.testing.Layer):
-    # Rope ZCML must be setup before Plone portal local registry is hooked to a
-    # global registry
-    defaultBases = (PLONE_ROPE_ZCML_FIXTURE, plone.app.testing.PLONE_FIXTURE)
-
-    def setUp(self):
-        from plone.app.testing import helpers
-
-        with helpers.ploneSite() as portal:
-            helpers.applyProfile(portal,
-                'collective.rope.tests:ropeonat')
-
-PLONE_ROPE_FIXTURE = PloneRope(name="PloneRope")
+PLONE_ROPE_FIXTURE = plone.app.testing.PloneWithPackageLayer(
+    name="PLONE_ROPE_FIXTURE",
+    zcml_filename="testingplone.zcml",
+    zcml_package=collective.rope,
+    gs_profile_id='collective.rope.tests:ropeonat'
+    )
 
 PTC_PLONE_ROPE_INTEGRATION = plone.app.testing.layers.IntegrationTesting(
     bases=(PLONE_ROPE_FIXTURE, ),
-    name='PTC:PloneRope:Integration')
+    name='PTC_PLONE_ROPE_INTEGRATION')
 
 PTC_PLONE_ROPE_FUNCTIONAL = plone.app.testing.layers.FunctionalTesting(
     bases=(PLONE_ROPE_FIXTURE, ),
-    name='PTC:PloneRope:Functional')
+    name='PTC_PLONE_ROPE_FUNCTIONAL')
